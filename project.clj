b51137b2a3209ec411ae26474b6bccb6144b0c1f
Tienson Qin <tiensonqin@gmail.com>
2023-09-19 16:41:27 +0800

    Initial implementation for cljs storage support

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "Fast B-tree based persistent sorted set for Clojure/Script"
   :license {:name "MIT"}
   :url "https://github.com/tonsky/persistent-sorted-set"
-  
+
   :dependencies
   [[org.clojure/clojure       "1.11.1"  :scope "provided"]
    [org.clojure/clojurescript "1.11.60" :scope "provided"]]
-  
+
   :plugins
   [[lein-cljsbuild "1.1.7"]]
 
@@ -14,15 +14,15 @@
   :java-source-paths ["src-java"]
   :test-paths        ["test-clojure"]
 
-  :javac-options ["-Xlint:unchecked" "-Xlint:-options" "-target" "8" "-source" "8" "-bootclasspath" ~(str (or (System/getenv "JAVA8_HOME") (throw (Exception. "Please set JAVA8_HOME"))) "/jre/lib/rt.jar")]
+  ;; :javac-options ["-Xlint:unchecked" "-Xlint:-options" "-target" "8" "-source" "8" "-bootclasspath" ~(str (or (System/getenv "JAVA8_HOME") (throw (Exception. "Please set JAVA8_HOME"))) "/jre/lib/rt.jar")]
   :jvm-opts ["-ea"]
 
   :profiles
   {:1.9
-   {:dependencies 
+   {:dependencies
     [[org.clojure/clojure       "1.9.0"   :scope "provided"]
      [org.clojure/clojurescript "1.9.946" :scope "provided"]]}}
-   
+
   :deploy-repositories
   {"clojars"
    {:url "https://clojars.org/repo"
